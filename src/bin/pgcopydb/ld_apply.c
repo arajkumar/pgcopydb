7b6d0c2850c174003cfb451357b45d7db9120d8f
Arunprasad Rajkumar <ar.arunprasad@gmail.com>
2023-09-18 15:04:09 +0000

    Exit from pipeline for WAL SWITCH command
    Signed-off-by: Arunprasad Rajkumar <ar.arunprasad@gmail.com>

--- src/bin/pgcopydb/ld_apply.c
+++ src/bin/pgcopydb/ld_apply.c
@@ -614,6 +614,10 @@ stream_apply_sql(StreamApplyContext *context,
 					  LSN_FORMAT_ARGS(metadata->lsn));
 
 			context->previousLSN = metadata->lsn;
+			/*
+			 * exit from pipeline before committing.
+			 */
+			pgsql_exit_pipeline_mode(pgsql);
 
 			break;
 		}
