8558c9b634867cfcbe21092055446d370c865528
Harkishen-Singh <harkishensingh@hotmail.com>
2023-11-02 20:56:13 +0530

    docs: add usage instructions in script
    Signed-off-by: Harkishen-Singh <harkishensingh@hotmail.com>

--- src/bin/timescale/housekeeping.py
+++ src/bin/timescale/housekeeping.py
@@ -1,4 +1,32 @@
 #!/usr/bin/env python3
+#
+# Housekeeping is a script designed to remove stale WAL and transaction files (.sql & .json)
+# from the `$PGCOPYDB_DIR/cdc` directory. These files have already been applied to the target
+# and hence are no longer needed.
+#
+# The script operates by maintaining an ordered list of all files in the working directory,
+# sorted by their hexadecimal names. It then retrieves the current WAL file from the source DB
+# and locates its index within the ordered list of files. Once the index is identified,
+# the script removes all preceding files, retaining a buffer of three files for safety of large txns.
+# This operation is performed every 5 minutes by default.
+#
+# To function correctly, housekeeping script requires certain environment variables:
+#
+# ```shell
+# # Required.
+# export PGCOPYDB_SOURCE_PGURI=''
+# export PGCOPYDB_TARGET_PGURI=''
+# export PGCOPYDB_DIR=''
+#
+# # Optional.
+# export HOUSEKEEPING_INTERVAL=300 # Seconds.
+# ```
+#
+# ## Usage
+# ```shell
+# python housekeeping.py
+# ```
+
 import os
 import json
 import subprocess
