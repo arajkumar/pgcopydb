704bdb5b21f9cfcd8c0fbff6d0b1a16364c366db
Harkishen-Singh <harkishensingh@hotmail.com>
2024-04-18 17:03:36 +0530

    feat: setup actions for e2e tests
    Signed-off-by: Harkishen-Singh <harkishensingh@hotmail.com>

--- integration/tests/main.rs
+++ integration/tests/main.rs
@@ -2,6 +2,11 @@ use tempfile::tempdir;
 use test_common::{timescaledb, HasConnectionString, PgVersion};
 use testcontainers::{clients::Cli, core::WaitFor, images::generic::GenericImage, RunnableImage};
 
+/// HOST is a magical IP that works as a replacement for
+/// host.docker.internal for mac and at the same time allows
+/// containers on Github actions to access other host containers.
+static HOST: &str = "172.17.0.1";
+
 #[test]
 fn test_snapshot_creation() {
     let _ = pretty_env_logger::try_init();
@@ -16,20 +21,21 @@ fn test_snapshot_creation() {
     let target_container = docker.run(timescaledb(PgVersion::PG15));
 
     let temp_dir = tempdir().unwrap();
-    let image = GenericImage::new("timescale/live-migration", "main")
+
+    let image = GenericImage::new("local/live-migration", "dev")
         .with_env_var(
             "PGCOPYDB_SOURCE_PGURI",
             source_container
                 .connection_string()
                 .as_str()
-                .replace("127.0.0.1", "host.docker.internal"),
+                .replace("127.0.0.1", HOST),
         )
         .with_env_var(
             "PGCOPYDB_TARGET_PGURI",
             target_container
                 .connection_string()
                 .as_str()
-                .replace("127.0.0.1", "host.docker.internal"),
+                .replace("127.0.0.1", HOST),
         )
         .with_volume(temp_dir.path().to_str().unwrap(), "/opt/timescale/ts_cdc")
         .with_wait_for(WaitFor::message_on_stdout(
@@ -37,5 +43,5 @@ fn test_snapshot_creation() {
         ));
 
     let target = RunnableImage::from((image, vec![String::from("snapshot")]));
-    let _live_migration_snapshot = docker.run(target);
+    docker.run(target);
 }
