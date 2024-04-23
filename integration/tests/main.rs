33543cc71ee6de1b0fe11e2cec1d1d58cf295fc9
James Guthrie <jguthrie@timescale.com>
2024-04-23 10:16:05 +0200

    fix: use docker networking in integration tests
    The workaround with directing traffic from within the docker container
    to the 172.17.0.1 IP has a couple of shortcomings:
    1. It assumes that the docker0 bridge always has the same address
    2. It assumes that firewall rules will allow the traffic
    
    By using docker's integrated networking, we can remove the workaround
    and rely on docker to "just make it work".

--- integration/tests/main.rs
+++ integration/tests/main.rs
@@ -1,11 +1,12 @@
+use rand::{thread_rng, Rng};
 use tempfile::tempdir;
-use test_common::{timescaledb, HasConnectionString, PgVersion};
+use test_common::{timescaledb, InternalConnectionString, PgVersion};
 use testcontainers::{clients::Cli, core::WaitFor, images::generic::GenericImage, RunnableImage};
 
-/// HOST is a magical IP that works as a replacement for
-/// host.docker.internal for mac and at the same time allows
-/// containers on Github actions to access other host containers.
-static HOST: &str = "172.17.0.1";
+fn generate_test_network_name() -> String {
+    let mut rng = thread_rng();
+    format!("network-{}", rng.gen_range(10_000..=99_999))
+}
 
 #[test]
 fn test_snapshot_creation() {
@@ -13,35 +14,35 @@ fn test_snapshot_creation() {
 
     let docker = Cli::default();
 
-    let source_image = RunnableImage::from((
-        timescaledb(PgVersion::PG15),
-        vec![String::from("-c"), String::from("wal_level=logical")],
-    ));
-    let source_container = docker.run(source_image);
-    let target_container = docker.run(timescaledb(PgVersion::PG15));
+    let network_name = generate_test_network_name();
+
+    let source_container = docker.run(
+        RunnableImage::from((
+            timescaledb(PgVersion::PG15),
+            vec![String::from("-c"), String::from("wal_level=logical")],
+        ))
+        .with_network(&network_name),
+    );
+    let target_container =
+        docker.run(RunnableImage::from(timescaledb(PgVersion::PG15)).with_network(&network_name));
 
     let temp_dir = tempdir().unwrap();
 
     let image = GenericImage::new("local/live-migration", "dev")
         .with_env_var(
             "PGCOPYDB_SOURCE_PGURI",
-            source_container
-                .connection_string()
-                .as_str()
-                .replace("127.0.0.1", HOST),
+            source_container.internal_connection_string().as_str(),
         )
         .with_env_var(
             "PGCOPYDB_TARGET_PGURI",
-            target_container
-                .connection_string()
-                .as_str()
-                .replace("127.0.0.1", HOST),
+            target_container.internal_connection_string().as_str(),
         )
         .with_volume(temp_dir.path().to_str().unwrap(), "/opt/timescale/ts_cdc")
         .with_wait_for(WaitFor::message_on_stdout(
             "You can now start the migration process",
         ));
 
-    let target = RunnableImage::from((image, vec![String::from("snapshot")]));
+    let target =
+        RunnableImage::from((image, vec![String::from("snapshot")])).with_network(&network_name);
     docker.run(target);
 }
